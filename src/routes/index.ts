41aa95d50118fd7b1a64fe2802c819af3462289a
thangdepzai12-dev <thangkhaiyt24@gmail.com>
2026-06-22 20:35:58 +0700

    Revert "CRUD FOOD storage locations, food status,storage suggestion"
    This reverts commit 3718eeef296c9b9ee25d67f65daf932c07700303.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,6 @@ import healthRouter from './health';
 import authRouter from './authRoutes';
 import userRouter from './userRoutes';
 import subscriptionRouter from './subscriptionRoutes';
-import foodRouter from './foodRoutes';
-import storageRouter from './storageRoutes';
 
 const apiRouter = Router();
 
@@ -13,8 +11,5 @@ apiRouter.use('/health', healthRouter);
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/users', userRouter);
 apiRouter.use('/subscriptions', subscriptionRouter);
-apiRouter.use('/foods', foodRouter);
-apiRouter.use('/storage-locations', storageRouter);
-apiRouter.use('/storage', storageRouter);
 
 export default apiRouter;
