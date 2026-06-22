3718eeef296c9b9ee25d67f65daf932c07700303
thangdepzai12-dev <thangkhaiyt24@gmail.com>
2026-06-22 20:24:36 +0700

    CRUD FOOD storage locations, food status,storage suggestion

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,8 @@ import healthRouter from './health';
 import authRouter from './authRoutes';
 import userRouter from './userRoutes';
 import subscriptionRouter from './subscriptionRoutes';
+import foodRouter from './foodRoutes';
+import storageRouter from './storageRoutes';
 
 const apiRouter = Router();
 
@@ -11,5 +13,8 @@ apiRouter.use('/health', healthRouter);
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/users', userRouter);
 apiRouter.use('/subscriptions', subscriptionRouter);
+apiRouter.use('/foods', foodRouter);
+apiRouter.use('/storage-locations', storageRouter);
+apiRouter.use('/storage', storageRouter);
 
 export default apiRouter;
