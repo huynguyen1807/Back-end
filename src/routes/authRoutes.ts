33ec69e253faf6bee7cdb0752d9cadef0d3a0298
minhthong-git <thongnguyenminh284@gmail.com>
2026-07-17 08:17:44 +0700

    forgot password, edit profile

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { register, login, getMe, verifyOTP, resendOTP, googleLogin } from '../controllers/authController';
+import { register, login, getMe, verifyOTP, resendOTP, googleLogin, forgotPassword, resetPassword } from '../controllers/authController';
 import { protect } from '../middleware/authMiddleware';
 
 const router = Router();
@@ -9,6 +9,8 @@ router.post('/verify-otp', verifyOTP);
 router.post('/resend-otp', resendOTP);
 router.post('/login', login);
 router.post('/google', googleLogin);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 router.get('/me', protect, getMe);
 
 export default router;
